2bf622770932766640a74cf63d997b9179ad4488
Jess <jessgaspardev@gmail.com>
2026-04-16 19:19:40 +0100

    chore: adjust content collections schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,15 +4,13 @@ import { z } from 'astro/zod'
 
 const projects = defineCollection({
     loader: glob({pattern: "src/content/projects/**/*.md"}),
-    schema: ({image}) => z.object({
+    schema: z.object({
         id: z.number(),
         title: z.string().max(50),
-        slug: z.string(),
-        category: z.preprocess(
-                    (val) => (Array.isArray(val) ? val : [val]),
-                    z.array(z.enum(["Placeholder 1", "Placeholder 2"]))),
+        tools: z.preprocess(
+            (val) => (Array.isArray(val) ? val : [val]),
+            z.array(z.enum(["TypeScript", "LIVE", "HTML", "JavaScript", "React Native", "Backend API", "Node.js", "Docker", "Tailwind"]))), // change categories here
         year: z.string().max(4),
-        featuredImage: image().optional(),
         liveSite: z.url().optional(),
         github: z.url().optional(),
         description: z.string().max(350),
@@ -23,12 +21,11 @@ const projects = defineCollection({
 
 const blog = defineCollection({
     loader: glob({pattern: "src/content/blog/**/*.md"}),
-    schema: ({image}) => z.object({
+    schema: z.object({
         id: z.number(),
         slug: z.string().max(50),
         title: z.string().max(50),
         publishedDate: z.date(),
-        image: image(),
         category: z.enum(["Placeholder 1", "Placeholder 2"]), // change and add blog categories here
         isDraft: z.boolean()
     })
